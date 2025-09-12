73dbe786d0a06bd0f17d54bbd4e27ebbc820488f
Jan Thiel <jan.thiel@emmet-software-labs.com>
2025-09-12 13:37:07 +0200

    wip: showing vehicles from backend

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VehicleService } from './vehicle.service';
+import { Vehicle } from "./vehicle";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Vehicle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('azubi-project');
+  vehicleService = inject(VehicleService);
+  vehicles = this.vehicleService.fetchVehicleApi();
 }
