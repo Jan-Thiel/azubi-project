4a707e3505e592531c0ed01975f0453cf8a1fa09
Jan Thiel <jan.thiel@emmet-software-labs.com>
2025-11-24 13:26:25 +0100

    feat: changing the email and password of a user

--- src/app/service/customer.service.ts
+++ src/app/service/customer.service.ts
@@ -1,38 +1,68 @@
-import { inject, Injectable } from '@angular/core'
+import { EventEmitter, Inject, inject, Injectable, PLATFORM_ID } from '@angular/core'
 import { HttpClient, HttpParams } from '@angular/common/http'
 import { Customer } from '../model/customer.model'
-import { CookieService } from './cookie.service'
+import { SsrCookieService } from 'ngx-cookie-service-ssr'
+import { Auth } from '../model/auth.model'
+import { isPlatformBrowser } from '@angular/common'
+import { Observable } from 'rxjs'
+import { tap } from 'rxjs/operators'
+import { Store } from '@ngrx/store'
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomerService {
-  cookieService = inject(CookieService)
-  constructor(private http: HttpClient) {}
+  cookieService = inject(SsrCookieService)
+  store = inject(Store)
+  platformId
 
-  login(email: string, password: string) {
-    let parameters = new HttpParams().append('email', email).append('password', password)
-    let reply = this.http.get<Customer>('http://localhost:8080/api/customers/login', {
-      params: parameters,
-    })
-    reply.subscribe(customer => {
-      console.log(customer)
-      this.cookieService.setCookie('userId', customer.customerId, 5, '', document)
-    })
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) platformId: Object,
+  ) {
+    this.platformId = platformId
   }
 
-  getUserData() {
-    let parameters = new HttpParams().append('id', this.cookieService.getCookie('userId', document))
-    let repl = this.http.get<Customer>('http://localhost:8080/api/customers/user', {
-      params: parameters,
+  loginChange = new EventEmitter()
+
+  async login(email: string, password: string) {
+    try {
+      let reply = this.http.post<Auth>('http://localhost:8080/api/customers/login', {
+        email: email,
+        password: password,
+        withCredentials: true,
+      })
+
+      reply.subscribe(auth => {
+        // Set all necessary cookies when logged in
+        this.cookieService.set('userId', String(auth.customer.customerId), { expires: 5 })
+        this.cookieService.set('refreshToken', auth.refreshToken, { expires: 5 })
+        // This check is necessary to avoid runtime errors due to SSR
+        if (isPlatformBrowser(PLATFORM_ID)) {
+          sessionStorage.setItem('jwttoken', auth.token)
+        }
+        // Notify the Navbar of a login
+        this.loginChange.emit(true)
+      })
+    } catch (error) {
+      this.loginChange.emit(false)
+      throw error
+    }
+  }
+
+  getUserData(): Observable<Customer> {
+    return this.http.get<Customer>('http://localhost:8080/api/customers/user', {
+      withCredentials: true,
     })
-    return repl
   }
 
   signup(email: string, password: string, firstName: string, surname: string): boolean {
     let checkParameters = new HttpParams().append('email', email)
     this.http
-      .get<boolean>('http://localhost:8080/api/customers/checkEmail', { params: checkParameters })
+      .get<boolean>('http://localhost:8080/api/customers/checkEmail', {
+        params: checkParameters,
+        withCredentials: true,
+      })
       .subscribe(check => {
         if (check) {
           let parameters = new HttpParams()
@@ -40,17 +70,40 @@ export class CustomerService {
             .append('password', password)
             .append('firstName', firstName)
             .append('surname', surname)
-          let reply = this.http.get<Customer>('http://localhost:8080/api/customers/signup', {
+          let reply = this.http.get<Auth>('http://localhost:8080/api/customers/signup', {
             params: parameters,
+            withCredentials: true,
           })
-          reply.subscribe(customer =>
-            this.cookieService.setCookie('userid', customer.customerId, 5, '', document),
-          )
+          reply.subscribe(auth => {
+            this.login(email, password).then()
+          })
           return true
         } else {
           return false
         }
       })
     return false
+  }
+
+  changeEmail(email: string) {
+    this.http.post('http://localhost:8080/api/customers/changeEmail', {
+      email: email,
+      withCredentials: true,
+    })
+  }
+
+  changePassword(password: string, newPassword: string) {
+    this.http.post('http://localhost:8080/api/customers/changePassword', {
+      password: password,
+      newPassword: newPassword,
+      withCredentials: true,
+    })
+  }
+
+  ngOnInit() {
+    this.loginChange.emit(false)
+    if (isPlatformBrowser(PLATFORM_ID)) {
+      this.cookieService.set('isAnon', 'false', { expires: 1 })
+    }
   }
 }
