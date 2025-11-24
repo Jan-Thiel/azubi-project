c2a32f33158bc79f07f1f3ddf566494615580144
Jan Thiel <jan.thiel@emmet-software-labs.com>
2025-11-24 12:30:03 +0100

    feat: page for error code 404

---
+++ src/app/feature-no-page/no-page/no-page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-no-page',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './no-page.html',
+  styleUrl: './no-page.css'
+})
+export class NoPage {
+
+}
